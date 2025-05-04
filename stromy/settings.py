d5a9b73ead9ceb738a3a2b0be982a456e91b6625
Adam <adamseidlerr.as@gmail.com>
2025-05-04 13:04:54 +0200

    úprava souboru setting.py -> přidání ALLOWED_HOSTS = ['atlas-stromu.onrender.com']

--- stromy/settings.py
+++ stromy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kjp=y3owa20s9c&0#j6@l_0$cuqapw4^0v%68h0_n9ofjuj%0a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['atlas-stromu.onrender.com']
 
 
 # Application definition
